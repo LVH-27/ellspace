c12ab24722e21f390c7af9ca49de3c0461028630
LukaMesaric <luka.mesaric1@gmail.com>
2017-08-13 14:37:12 +0200

    Used for populating the GenreList table (not finished).

---
+++ Database/PopulateTableGenreList.sql
@@ -0,0 +1,18 @@
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Unknown');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Other');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Komedija');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Tragedija');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Drama');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Ep');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Pripovijetka');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Novela');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Roman');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Zbirka pjesama');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Časopis');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Esej');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Putopis');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Basna');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Bajka');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Životopis');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Udžbenik');
+INSERT INTO "GenreList" VALUES(DEFAULT, 'Enciklopedija');
