2446721afe26e3f26b5eaa6fd4b003c229118c32
LukaMesaric <luka.mesaric1@gmail.com>
2017-08-13 14:38:08 +0200

    Used for populating the LanguageList table.

---
+++ Database/PopulateTableLanguageList.sql
@@ -0,0 +1,39 @@
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Unknown');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Other');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Hrvatski');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Engleski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Njemački');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Srpski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Češki');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Slovački');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Poljski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Ruski');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Francuski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Talijanski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Španjolski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Portugalski');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Danski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Nizozemski');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Finski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Norveški');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Švedski');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Mađarski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Rumunjski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Bugarski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Makedonski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Albanski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Grčki');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Turski');
+
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Kineski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Japanski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Arapski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Hindski');
+INSERT INTO "LanguageList" VALUES(DEFAULT, 'Pandžapski');
