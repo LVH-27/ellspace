18fe04feb3062c918e24d0f622df6a1f22f66dcf
LukaMesaric <luka.mesaric1@gmail.com>
2017-08-17 19:47:24 +0200

    Added Python script for generating SQL file to set up the entire database and populate everything.

---
+++ Database/Generator_PopulateAll.py
@@ -0,0 +1,43 @@
+## Author: Luka Mesarić
+
+## Generira "PopulateAll.sql" file. Overwrita postojeći,
+## ali se slobodno može pokretat ponovno i ponovno jer se generirani
+## sql ne smije mijenjat. Tj. ako treba, radimo to preko ovog generatora.
+## Tko se hoće žalit da je loše napisano, slobodno može,
+## ali ovo dovoljno brzo radi :P
+
+
+def main():
+
+    export = open("PopulateAll.sql", "w")
+
+    import_list = ["ModelKnjiznica_postgres_create.sql",
+                   "PopulateTableCity.sql",
+                   "PopulateTableEventTypeList.sql",
+                   "PopulateTableGenreList.sql",
+                   "PopulateTableLanguageList.sql",
+                   "PopulateTableLocationList.sql"]
+
+    FINAL = ""
+    
+    for file in import_list:
+        imp = open(file, "r")
+        text = imp.read()
+        imp.close()
+        FINAL += text + '\n\n'
+
+    export.write(FINAL)
+
+
+    export.close()
+
+    
+    print('\nSQL file successfully generated.\n')
+
+    return
+
+
+
+main()
+
+
