fc00adc02721555860df69e92e07fd5ae88f7e5a
unknown <luka.mesaric1@gmail.com>
2017-09-11 18:37:22 +0200

    Started on AddBook feature.

--- Knjiznica/src/knjiznica/view/AddBookView.java
+++ Knjiznica/src/knjiznica/view/AddBookView.java
@@ -1,10 +1,41 @@
 package knjiznica.view;
 
+import java.io.IOException;
 import java.sql.SQLException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import knjiznica.model.ViewProvider;
 
 public class AddBookView {
 	
+	@FXML
+	private Button addButton;
+	
+	@FXML
+	private Button backButton;
+	
 	public void initialize() throws SQLException {
-
+		Image imageAddButton = new Image(getClass().getResourceAsStream("../resources/add-button.png"));
+		addButton.setGraphic(new ImageView(imageAddButton));
+		addButton.setId("transparentButton");
+		
+		Image imageBackButton = new Image(getClass().getResourceAsStream("../resources/back-button.png"));
+		backButton.setGraphic(new ImageView(imageBackButton));
+		backButton.setId("transparentButton");
+	}
+	
+	@FXML
+	private void activateBack() throws IOException {
+		BorderPane clientsMenu = (BorderPane) ViewProvider.getView("clientsMenu");
+		((BorderPane) ViewProvider.getView("mainScreen")).setCenter(clientsMenu);
+	}
+	
+	@FXML
+	private void activateAdd() {
+		
 	}
 }
