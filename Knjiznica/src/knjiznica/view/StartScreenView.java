c5161c47ba3227fcf3710248e487262e549d2c15
unknown <luka.mesaric1@gmail.com>
2017-09-15 18:13:40 +0200

    Changes to colour and names for hover Label methods.

--- Knjiznica/src/knjiznica/view/StartScreenView.java
+++ Knjiznica/src/knjiznica/view/StartScreenView.java
@@ -127,7 +127,7 @@ public class StartScreenView {
 	
 	@FXML
 	private void activateShareOnMouseEntered() {
-		nameOnHoverLabel.setText("Share Book");
+		nameOnHoverLabel.setText("Book Management");
 	}
 	
 	@FXML
