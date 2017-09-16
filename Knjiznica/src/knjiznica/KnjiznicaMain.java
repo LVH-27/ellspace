34eb8cb1b6505f6af708fa074c040fbe22d72a8c
unknown <luka.mesaric1@gmail.com>
2017-09-16 13:06:19 +0200

    Removed unused code.

--- Knjiznica/src/knjiznica/KnjiznicaMain.java
+++ Knjiznica/src/knjiznica/KnjiznicaMain.java
@@ -15,34 +15,25 @@ public class KnjiznicaMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		
-		try {
-			primaryStage.hide();
-
-			primaryStage.setMaximized(true);
-			
-			
+		try {			
 			ViewProvider.setView("primaryStage", primaryStage);
 			
 			StackPane root = (StackPane) FXMLLoader.load(
 					getClass().getResource("view/Main-view.fxml"));
-			//FIXME Screen flashes during loading and switching to maximized window.
-			//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 			Scene scene = new Scene(root);
-					//screenSize.getWidth(), screenSize.getHeight());
 			
 			MaskerPane mask = new MaskerPane();
 			ViewProvider.setView("mask", mask);
 			ViewProvider.setView("stackPane", root);
 			
 			MainView main = (MainView) ViewProvider.getView("main");
-			
 			main.start();
 			
 			scene.getStylesheets().add(getClass().getResource(
 					"../resources/BackgroundStyle.css").toExternalForm());
 			
-			
-
+			primaryStage.hide();
+			primaryStage.setMaximized(true);
 			primaryStage.setTitle("ICM SoC 17 - Library");
 			primaryStage.setScene(scene);
 			primaryStage.show();
